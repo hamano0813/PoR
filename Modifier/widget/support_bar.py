fc857054d14884b267c1ed41f916c967480c3473
HamanoTaiki <hamano0813@gmail.com>
2021-08-04 13:30:30 +0800

    #22.新增支援显示进度条

---
+++ Modifier/widget/support_bar.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from PySide6.QtWidgets import QProgressBar
+
+
+class SupportBar(QProgressBar):
+    def __init__(self, text: str):
+        QProgressBar.__init__(self, parent=None)
+        self.setTextVisible(True)
+        self.setFormat(text)
+        self.setMaximumSize(100, 25)
+        self.setStyleSheet('''
+        QProgressBar {text-align: center; font: bold; border: 1px solid Grey; border-radius: 1px;}
+        QProgressBar::chunk {background-color: #05B8CC; width: 1px;}
+        ''')
+
+    def refresh(self):
+        value = self.value()
+        self.setValue(self.maximum())
+        self.setValue(self.minimum())
+        self.setValue(value)
+
+    def set_value(self, value: int):
+        if value < self.minimum():
+            self.setValue(self.minimum())
+        elif value > self.maximum():
+            self.setValue(self.maximum())
+        else:
+            self.setValue(value)
