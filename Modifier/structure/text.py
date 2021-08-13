4970161824de7c6596014bb32f7baa12385b811c
HamanoTaiki <hamano0813@gmail.com>
2021-08-13 14:46:17 +0800

    #47.文本类增加调试代码

--- Modifier/structure/text.py
+++ Modifier/structure/text.py
@@ -11,7 +11,18 @@ class Text:
         if (temp > 0x80000000) and (temp < 0x81800000):
             return cls.get_text(temp)
         text = ''
+        # _offset = offset
         while read_byte(offset):
             text += read_bytes(offset, 1).decode()
             offset += 1
+        # print(f"0x{_offset:08X}: self.tr('{text}'),")
         return text
+
+
+# if __name__ == '__main__':
+#     from dolphin_memory_engine import hook
+#
+#     hook()
+#     for i in range(32):
+#         t = Text.get_text(0x8080A614 + 0x18 * i)
+        # print(f"'{t}': self.tr('{t}'),")
